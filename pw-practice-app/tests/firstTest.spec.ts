2b14f3cbadefd00ceecfd05bab6d4f83218b82d1
S M HEMEL <smhemel.eu@gmail.com>
2025-11-10 02:37:43 +0600

    Write some demo test cases to understand Hooks and Flow Control

--- pw-practice-app/tests/firstTest.spec.ts
+++ pw-practice-app/tests/firstTest.spec.ts
@@ -1,7 +1,66 @@
 import { test } from '@playwright/test';
 
-test('the first test', async ({page}) => {
+// Hook
+test.beforeAll(() => {
+    console.log('This is the beginning of the test suite');
+});
+
+// Hook
+test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200');
     await page.getByText('Forms').click();
+});
+
+test('the first test', async ({page}) => {
     await page.getByText('Form Layouts').click();
 });
+
+test('navigate to datepicker page', async ({page}) => {
+    await page.getByText('Datepicker').click();
+});
+
+// Hook
+test.afterEach(() => {
+    console.log('Test completed');
+});
+
+// Hook
+test.afterAll(() => {
+    console.log('All tests are done!');
+});
+
+// Below we have define two test suites where we removed this(await page.goto('http://localhost:4200');) line from beforeEach hooks.
+// Because we already declared it inside a beforeEach hook on the top of this file.
+
+// With the describe, we can add skip or only to a group of tests.
+// Example: test.describe.only or test.describe.skip
+// skip - will skip all tests inside the describe block
+// only - will run only the tests inside the describe block
+test.describe('Suite 1', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText('Charts').click();
+    });
+
+    test('test 1', async ({page}) => {
+        await page.getByText('Chart Layouts').click();
+    });
+
+    test('test 2', async ({page}) => {
+        await page.getByText('Datepicker').click();
+    });
+});
+
+test.describe('Suite 1', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText('Admin').click();
+    });
+
+    test('test 1', async ({page}) => {
+        await page.getByText('Admin Layouts').click();
+    });
+
+    test('test 2', async ({page}) => {
+        await page.getByText('Datepicker').click();
+    });
+});
+
